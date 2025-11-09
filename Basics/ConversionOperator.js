6ad7548c15e247fd3d86cad4194fcbc0484ea9d0
kunal <kunalgarg6648@gmail.com>
2025-11-09 15:29:26 +0530

    Conversions

--- Basics/ConversionOperator.js
+++ Basics/ConversionOperator.js
@@ -1,10 +1,10 @@
 let score = "33abc";
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueofNumber = Number(score);
-console.log(typeof valueofNumber);
-console.log(valueofNumber);
+//console.log(typeof valueofNumber);
+//console.log(valueofNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,4 +13,28 @@ console.log(valueofNumber);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//// ***************************** Operation *****************************??
+
+let value = 3;
+let negvalue = -value;
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log("2"+"2");
+// console.log("2"+2);
+// console.log("2"-2);
+// console.log("2"*2);
+// console.log("2"/2);
+
+let str1 = "Hello ";    
+let str2 = "World";
+let str3 = str1 + str2;
+// console.log(str3);
+
+console.log("1"+"2");
+console.log(1+"2");
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); //32
+
 
--- first.js
+++
@@ -1,2 +0,0 @@
-console.log("kunal");
-console.log("Garg");
